c7dd70bbc5b3d87f2541d758ee90cf49d0a6bd5b
Gabriel.Barbosa <gfelipebarbosa@yahoo.com>
2020-12-23 11:58:46 -0300

    Continuação da anterior

--- JavaScript/Modulo D Condições em JS/Aula Exercicio 02/script.js
+++ JavaScript/Modulo D Condições em JS/Aula Exercicio 02/script.js
@@ -1,1 +1,20 @@
-alert('Olá')
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    // var hora = data.getHours()
+    var hora = 22
+    msg.innerHTML = `Agora são ${hora} horas. `
+    console.log(hora)
+    if(hora >= 0 && hora < 12) {
+        // Bom Dia
+        img.src = 'img/manha.jpg'
+    }else if ( hora >= 12 && hora < 18 ){
+        // Boa Tarde
+        img.src = 'img/tarde.jpg'
+    }else{
+        // Boa Noite
+        img.src = 'img/noite.jpg'
+    }
+
+}
